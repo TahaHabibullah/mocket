04183e9a1cd2f606c785a275b4b450045ceab621
Taha <th.2190@gmail.com>
2024-07-23 01:32:24 -0500

    Adj logo/repo for all screens, added links

--- mocket-frontend/src/components/MocketNavBar.js
+++ mocket-frontend/src/components/MocketNavBar.js
@@ -9,12 +9,14 @@ const MocketNavBar = () => {
     const [results, setResults] = useState(null);
     return (
         <ul className="nav-bar">
-            <li><img src={logo} className="nav-bar-brand"/></li>
+            <a href="/"><img src={logo} className="nav-bar-brand"/></a>
             <li className="nav-bar-search">
                 <SymbolSearch setResults={setResults}/>
                 {results && results.hasOwnProperty("data") && <SymbolResultsList results={results}/>}
             </li>
-            <li><img src={github} className="nav-bar-repo"/></li>
+            <a href="https://github.com/TahaHabibullah/" target="_blank">
+                <img src={github} className="nav-bar-repo"/>
+            </a>
         </ul>
     );
 }
